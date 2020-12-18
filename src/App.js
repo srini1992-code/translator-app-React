1072f50cbdc5691db41b3bc56a1e6e3710d0d66c
Srinidhi S <srinidhis1992dec@gmail.com>
2020-12-18 08:29:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Field from "./components/field";
+import Languages from "./components/language";
+import Translate from "./components/translate";
+
+export default function App() {
+  const [language, setLanguage] = useState("ru");
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <h1 class="title">Translator App</h1>
+      <Field label="Enter English" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
+    </div>
+  );
+}
